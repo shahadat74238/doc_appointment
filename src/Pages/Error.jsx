70f6239b78afc877aa23d187dcc5ba29746761a6
shahadat74238 <shahadat74238@gmail.com>
2024-01-26 11:35:45 +0600

    Added error page

---
+++ src/Pages/Error.jsx
@@ -0,0 +1,10 @@
+
+const Error = () => {
+    return (
+        <div className="h-screen">
+            <img className="h-full w-full object-cover" src="https://img.freepik.com/free-vector/404-error-with-portals-concept-illustration_114360-7870.jpg" alt="" />
+        </div>
+    );
+};
+
+export default Error;
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -7,11 +7,13 @@ import Home from "../Pages/Home";
 import Dashboard from "../Pages/Dashboard";
 import Appointment from "../Pages/Appointment";
 import PrivetRouter from "./PrivetRouter";
+import Error from "../Pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+    errorElement: <Error />,
     children: [
       {
         index: true,
